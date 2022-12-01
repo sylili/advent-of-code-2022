442368186bbc8cd6af3574eb9f3c6cbbd58e039d
sylili <illesy.lilla@gmail.com>
2022-12-01 23:03:01 +0100

    solve day 1

--- src/1ii.js
+++ src/1ii.js
@@ -3,7 +3,10 @@ module.exports = (input) => {
 
   const packPerElf = elves.map((elf) => elf.split("\n").map((x) => +x));
   const sums = packPerElf.map((pack) => pack.reduce((sum, curr) => sum + curr));
-  const greatest = sums.reduce((prev, curr) => Math.max(prev, curr));
+  const sumOfFirst3 = sums
+    .sort((a, b) => b - a)
+    .slice(0, 3)
+    .reduce((a, b) => a + b);
 
-  return greatest;
+  return sumOfFirst3;
 };
